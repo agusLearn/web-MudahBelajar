ade35c3f68c3d340f5c5d578a1a123ceb4d5a61f
agusnindo <agusnindo@gmail.com>
2021-05-26 19:53:09 +0700

    sedikit perubahan pada menu mengarah ke profile

--- resources/views/adminPages/templates/header-admin.blade.php
+++ resources/views/adminPages/templates/header-admin.blade.php
@@ -41,8 +41,6 @@
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown"><img src="{{ asset('assets/img/user.png') }}" class="img-circle" alt="Avatar"> <span>Samuel</span> <i class="icon-submenu lnr lnr-chevron-down"></i></a>
                     <ul class="dropdown-menu">
                         <li><a href="#"><i class="lnr lnr-user"></i> <span>My Profile</span></a></li>
-                        <li><a href="#"><i class="lnr lnr-envelope"></i> <span>Message</span></a></li>
-                        <li><a href="#"><i class="lnr lnr-cog"></i> <span>Settings</span></a></li>
                         <li><a href="#"><i class="lnr lnr-exit"></i> <span>Logout</span></a></li>
                     </ul>
                 </li>
